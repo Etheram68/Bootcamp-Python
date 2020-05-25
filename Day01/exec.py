27b571dc111dd80d302877a70631a2c9f3448569
Etheram <francois681927@hotmail.fr>
2020-05-25 13:57:08 +0200

    Add security to function

--- Day01/exec.py
+++ Day01/exec.py
@@ -6,18 +6,19 @@
 #    By: frfrey <frfrey@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/05/25 12:49:35 by frfrey            #+#    #+#              #
-#    Updated: 2020/05/25 12:53:50 by frfrey           ###   ########lyon.fr    #
+#    Updated: 2020/05/25 13:56:30 by frfrey           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
-i = 0
-for arg in sys.argv:
-    if i == 1:
-        string = arg
-    elif i > 1:
-        string += " "
-        string += arg
-    i += 1
-print(string[::-1].swapcase())
+if (len(sys.argv) > 1):
+    i = 0
+    for arg in sys.argv:
+        if i == 1:
+            string = arg
+        elif i > 1:
+            string += " "
+            string += arg
+        i += 1
+    print(string[::-1].swapcase())
