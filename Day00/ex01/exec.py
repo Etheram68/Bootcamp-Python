7924239786e3ce5a63961daf29ffd0185ac68f82
Etheram <francois681927@hotmail.fr>
2020-05-25 14:24:32 +0200

    Modify struct of projet

--- Day00/ex01/exec.py
+++ Day00/ex01/exec.py
@@ -6,19 +6,12 @@
 #    By: frfrey <frfrey@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/05/25 12:49:35 by frfrey            #+#    #+#              #
-#    Updated: 2020/05/25 13:56:30 by frfrey           ###   ########lyon.fr    #
+#    Updated: 2020/05/25 14:20:17 by frfrey           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
 if (len(sys.argv) > 1):
-    i = 0
-    for arg in sys.argv:
-        if i == 1:
-            string = arg
-        elif i > 1:
-            string += " "
-            string += arg
-        i += 1
-    print(string[::-1].swapcase())
+    string = ' '.join(sys.argv[1:])[::-1].swapcase()
+    print(string)
